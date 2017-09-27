c849177061a9ce6de89f4b14424b7fad8a390677
VolchyokVladislav <volchyokvlad@tut.by>
2017-09-27 11:04:48 +0300

    ver 2.1

--- ATM1.py
+++ ATM1.py
@@ -19,14 +19,13 @@ class Repository(metaclass=ABCMeta):
 
 class Check(Repository):
     def __init__(self, password):
-        global number
         if password == Repository.users['Mark']['password']:
             print('change operation: \n 1)Checking \n 2)withdraw_money \n 3)eject_card')
-            number = input('your number: ')
         else:
             raise TypeError('try again')
 
     def test(self):
+        number = (input('your number: '))
         if int(number) == 1:
             return self.Checking()
         elif int(number) == 2:
