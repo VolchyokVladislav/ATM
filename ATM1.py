472737af1fe636c77aa4aac0034bcbf21f901172
VolchyokVladislav <volchyokvlad@tut.by>
2017-09-27 10:49:53 +0300

    ver 2.0

--- ATM1.py
+++ ATM1.py
@@ -4,8 +4,18 @@ class Repository(metaclass=ABCMeta):
     users = {'Mark': {'password': 1234, 'amount': 1000}}
 
     def Checking(self):
-        print(self.users['Mark']['amount'])
+        print('your amount:', self.users['Mark']['amount'])
 
+    def withdraw_money(self):
+        sum1 = input('enter sum: ')
+        if int(sum1) <= self.users['Mark']['amount']:
+            self.users['Mark']['amount'] -= int(sum1)
+            print('Take your money:', int(sum1))
+        else:
+            print('No money')
+
+    def eject_card(self):
+        print('Take your card')
 
 class Check(Repository):
     def __init__(self, password):
@@ -19,6 +29,11 @@ class Check(Repository):
     def test(self):
         if int(number) == 1:
             return self.Checking()
+        elif int(number) == 2:
+            return self.withdraw_money()
+        elif int(number) == 3:
+            return self.eject_card()
+
 
 
 
